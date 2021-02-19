7be8b1b9b8a412b6303fd39c5e15290c281b390f
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-19 20:19:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,18 +1,15 @@
-def display_board(board)
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-
-puts "Welcome to Tic Tac Toe!"
-
+def display_board (board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-
 end
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board (board)
 
+puts "Welcome to Tic Tac Toe!"
 puts "Where would you like to go?"
 
 def input_to_index (user_input)
