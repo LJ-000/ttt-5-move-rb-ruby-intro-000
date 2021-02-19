842ee0236d69cb62e188de691cd446936be1c50b
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-19 19:40:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,15 +1,10 @@
 def display_board(board)
+
+  puts "Welcome to Tic Tac Toe! Where would you like to go? #{value}."
+
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-  puts "Welcome to Tic Tac Toe! Where would you like to go? #{value}."
-
-
-board = [" ", " ", " "]
-def update_array_at_with(array, index, value)
-  array[index] = value
 end
