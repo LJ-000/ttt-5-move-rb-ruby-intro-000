ec8810bf7ebe2a5bddb528001c814861927e11a1
friendly-designer-db82 <nielsen.j.laura@gmail.com>
2021-02-19 20:46:27 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,16 +12,20 @@ display_board (board)
 puts "Welcome to Tic Tac Toe!"
 puts "Where would you like to go?"
 
-def input_to_index (index)
-  index.to_i -1
+def user_input
+  user_input = "5"
+  converted_input = input_to_index(user_input)
 end
 
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+expect {move(board, 4)}.to_not raise_error
+end
 
 def update_array_at_with (board, index, value)
   array [index] = value
 end
 
-value = "X"
+update_array_at_with (board, 5 , "X")
+end
 
-update_array_at_with (board, 0 , "X")
 display_board
